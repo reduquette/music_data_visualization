62ff3ceffdd1dbc2e375e54e015c210eab0faa9c
Rachel Duquette <duquetra@bc.edu>
2020-11-16 14:43:18 -0500

    setting up code architecture

---
+++ main.js
@@ -0,0 +1,14 @@
+import {} from "./histogram.js"
+import {} from "./recordPieChart.js"
+import {} from "./soundwave.js"
+import {} from "./dancers.js"
+
+//load data
+
+//make intro histogram
+
+//make record pie chart
+
+//make soundwave chart 
+
+//make dancers chaert
