6e474b75c5ae25576946937de30e059fd5326128
mcsoja <sojam@bc.edu>
2020-11-24 20:08:27 -0500

    scroll

--- histogram.js
+++ histogram.js
@@ -115,7 +115,43 @@ function updateBins(nBin){
         .attr("fill", function(d){ console.log("Y",height - y(d.length)); return myColor(height - y(d.length)) })
         
         
+        const ele = document.getElementById('histogramText');
 
+    ele.style.cursor = 'grab';
+
+    let pos = { top: 0, left: 0, x: 0, y: 0 };
+
+    const mouseDownHandler = function(e) {
+        ele.style.cursor = 'grabbing';
+        ele.style.userSelect = 'none';
+
+        pos = {
+            left: ele.scrollLeft,
+            top: ele.scrollTop,
+            x: e.clientX,
+            y: e.clientY,
+        };
+
+        document.addEventListener('mousemove', mouseMoveHandler);
+        document.addEventListener('mouseup', mouseUpHandler);
+    };
+
+    const mouseMoveHandler = function(e) {
+        const dx = e.clientX - pos.x;
+        const dy = e.clientY - pos.y;
+        ele.scrollTop = pos.top - dy;
+        ele.scrollLeft = pos.left - dx;
+    };
+
+    const mouseUpHandler = function() {
+        ele.style.cursor = 'grab';
+        ele.style.removeProperty('user-select');
+
+        document.removeEventListener('mousemove', mouseMoveHandler);
+        document.removeEventListener('mouseup', mouseUpHandler);
+    };
+
+    ele.addEventListener('mousedown', mouseDownHandler);
           
 
     u
@@ -151,7 +187,7 @@ d3.select("#nBin").on("input", function() {
 
 
 
-return{
+return {
   update
 }
 
