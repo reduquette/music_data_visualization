734c5439f8a05754480a01e8e454169f33f4b2b0
Jolene Lozano <lozanoja@bc.edu>
2020-12-03 20:58:39 -0500

    working on static visualizations

---
+++ soundwave-static.js
@@ -0,0 +1,152 @@
+var margin = {top: 50, right: 100, bottom: 30, left: 30},
+    width = 800 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svgStatic = d3.select("#soundwave-static")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")")
+
+d3.csv("averages.csv",d3.autoType).then(data => {
+      // List of groups (here I have one group per column)
+    var allGroup =  ["energy", "danceability", "acousticness"]
+
+    // Reformat the data: we need an array of arrays of {x, y} tuples
+    var dataReady = allGroup.map( function(grpName) { // .map allows to do something for each element of the list
+      return {
+        name: grpName,
+        values: data.map(function(d) {
+          return {decade: d.decade, value: +d[grpName]};
+        })
+      };
+    });
+
+    console.log(dataReady)
+
+        // A color scale: one color for each group
+    var myColor = d3.scaleOrdinal()
+      .domain(allGroup)
+      .range(["#E4E808", "#E805F6", "#4AE7E3"]);
+
+    // Add X axis --> it is a date format
+    var x = d3.scaleLinear()
+      .domain([1940, d3.max(data, function(d) { return +d.decade })])
+      .range([ 0, width ]);
+
+    svgStatic.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .attr("stroke", "white")
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain( [0,1])
+      .range([ height, 0 ]);
+      svgStatic.append("g")
+    .attr("stroke", "white")
+      .call(d3.axisLeft(y));
+
+    // var curve = d3.line().curve(d3.curveCardinal);
+    // var pathData = [curve(d.values)]
+
+    // Add the lines
+    var line = d3.line()
+      .x(function(d) { return x(+d.decade) })
+      .y(function(d) { return y(+d.value) })
+      .curve(d3.curveMonotoneX)
+
+
+    //Container for the gradients
+    var defs2 = svgStatic.append("defs");
+
+    //Filter for the outside glow
+    var filter2 = defs2.append("filter")
+        .attr("id","glow2");
+    filter2.append("feGaussianBlur")
+        .attr("stdDeviation","4")
+        .attr("result","coloredBlur");
+    var feMerge2 = filter2.append("feMerge");
+    feMerge2.append("feMergeNode")
+        .attr("in","coloredBlur");
+    feMerge2.append("feMergeNode")
+        .attr("in","SourceGraphic");
+
+
+    svgStatic.selectAll("myLines2")
+      .data(dataReady)
+      .enter()
+      .append("path")
+        .attr("d", function(d){ 
+            console.log("LINE", d.values)
+            return (line(d.values))
+        })
+        .attr("stroke", function(d){ console.log(myColor(d.name)); return myColor(d.name) })
+        .style("filter", "url(#glow)")
+        .style("stroke-width", 4)
+        .style("opacity", "0.4")
+
+        
+    
+    // Add the points
+    svgStatic
+      // First we need to enter in a group
+      .selectAll("myDots2")
+      .data(dataReady)
+      .enter()
+        .append('g')
+        .style("fill", function(d){ return myColor(d.name) })
+      // Second we need to enter in the 'values' part of this group
+      .selectAll("myPoints2")
+      .data(function(d){ return d.values })
+      .enter()
+      .append("circle")
+        .attr("cx", function(d) { return x(d.decade) } )
+        .attr("cy", function(d) { return y(d.value) } )
+        .attr("r", 5)
+        .attr("stroke", "white")
+
+    // Add a legend at the end of each line
+    svgStatic
+      .selectAll("myLabels")
+      .data(dataReady)
+      .enter()
+        .append('g')
+        .append("text")
+          .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; }) // keep only the last value of each time series
+          .attr("transform", function(d) { return "translate(" + x(d.value.decade) + "," + y(d.value.value) + ")"; }) // Put the text at the position of the last point
+          .attr("x", 12) // shift the text a bit more right
+          .text(function(d) { return d.name; })
+          .style("fill", function(d){ return myColor(d.name) })
+          .style("font-size", 15)
+          //tooltip
+
+
+    svgStatic.append("text")
+    .attr('x', 0)
+    .attr('y', -5)
+    .text("Levels")
+    .style("fill", "white")
+    .attr('font-size',13)
+    .attr('font-family','Lato')
+    .attr("transform", "rotate(90)");
+
+    svgStatic.append("text")
+    .attr('x', 650)
+    .attr('y', 350)
+    .text("Decades")
+    .style("fill", "white")
+    .attr('font-family','Lato')
+    .attr('font-size',13)
+
+
+})
+
+
+
+
+
+
