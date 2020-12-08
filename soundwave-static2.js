2f1696b0620e8085d12f62ded6a23817adf307eb
mcsoja <sojam@bc.edu>
2020-12-07 20:26:26 -0500

    fff

--- soundwave-static2.js
+++ soundwave-static2.js
@@ -4,7 +4,7 @@ var COUNTER2 = 0;
 function soundwaveStatic2(){
   var margin = {top: 15, right: 100, bottom: 35, left: 100},
     width = 800 - margin.left - margin.right,
-    height = 380 - margin.top - margin.bottom;
+    height = 320 - margin.top - margin.bottom;
   console.log("HERE")
   if (COUNTER2 == 0){
     console.log("HII")
@@ -197,6 +197,7 @@ d3.csv("averages.csv",d3.autoType).then(data => {
 
        d3.select("#highlight2")
        .attr("height", 0)
+       .attr('y', 40)
       .transition()
       .delay(1000)
       .duration(2000)
